7a5885efd00378c096e0d6b0620b0ee1f4624e95
Egor Abramov <coreegor@gmail.com>
2022-01-17 16:58:10 +0300

    test: add test_chown

--- tests/test_local_path.py
+++ tests/test_local_path.py
@@ -1,7 +1,12 @@
-
+from getpass import getuser
+import os
 from pathliberty import LocalPath
 
 def test_local():
     path = LocalPath('/foo/bar')
     assert (path / 'baz').parent == LocalPath('/foo/bar')
+
+def test_chown(tmp_path):
+    path = LocalPath(tmp_path)
+    path.chown(os.getuid(), os.getgid())
 
